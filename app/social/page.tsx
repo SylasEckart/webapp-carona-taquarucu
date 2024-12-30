b62ea2628c02501efcd304f99c7e799a229087dc
SylasEckart <eckartd.sylas@gmail.com>
2024-12-30 20:31:33 -0300

    pegando usuários reais

--- app/social/page.tsx
+++ app/social/page.tsx
@@ -7,16 +7,29 @@ import {
   Fade,
 } from '@mui/material'
 import { SocialFriendsList } from './socialList'
+import { useAppContext } from '../context/AppContext'
 
 // Mock friend data
 
 export default function SocialPage() {
 
+  const {isLoading,listUsers} = useAppContext();
+
+  if(isLoading) return <div>Loading...</div>
+
+  const newListUsers = listUsers.map((user) => {
+    return {
+      ...user,
+      isFriend: false
+    }
+  })
+  
+
   return (
         <Box sx={{ flexGrow: 1, overflow: 'auto', p: 2 }}>
           <Fade in={true} timeout={1000}>
             <Card elevation={0} sx={{ my: 2, borderRadius: 4 }}>
-              <SocialFriendsList />
+              <SocialFriendsList initialUsers={newListUsers} />
             </Card>
           </Fade>
         </Box>
